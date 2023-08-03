a0b191041df4d1bd3233a475f7dc565865619468
Artyom Tonoyan <arttonoyan@gmail.com>
2023-08-03 09:59:00 +0400

    Add StringEqualsSample

---
+++ src/TechTalks.SimpleStringConcatention.Benchmarks/StringEqualsSample.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace TechTalks.SimpleStringConcatention.Benchmarks;
+
+//Prefer string.Equals over ToLower/ToUpper
+public class StringEqualsSample
+{
+    private string str1 = "HELLO WORLD";
+    private string str2 = "hello world";
+
+    [Benchmark]
+    public bool Equals_OrdinalIgnoreCase() =>
+        string.Equals(str1, str2, StringComparison.OrdinalIgnoreCase);
+
+    [Benchmark]
+    public bool Compare_OrdinalIgnoreCase() =>
+        string.Compare(str1, str2, StringComparison.OrdinalIgnoreCase) == 0;
+
+    [Benchmark]
+    public bool ToLower() => str1.ToLower() == str2.ToLower();
+
+    [Benchmark]
+    public bool ToUpper() => str1.ToUpper() == str2.ToUpper();
+}
